6ea96e4bcaa3d518b6b6c1bcbc3a9f1413146bb0
Lucas7Alves <lucassalvess0909@gmail.com>
2024-10-09 16:45:51 -0300

    Inicio POO

---
+++ Atividades-POO/biblioteca.py
@@ -0,0 +1,19 @@
+class Pessoa:
+    def __init__(self, nome,peso,idade,comendo=False):
+        self.nome = nome
+        self.peso = peso
+        self.idade = idade
+
+    def andar(self):
+        print(f"{self.nome} está andando!")
+
+    def comer(self):
+        if
+        self.comer = True
+        print(f"{self.nome} está comendo!")
+
+    def dormir(self):
+        print(f"{self.nome} está dormindo!")
+
+    def pararDormir(self):
+        print(f"")
---
+++ Atividades-POO/main.py
@@ -0,0 +1,7 @@
+from biblioteca import *
+
+p1 = Pessoa("Lucas", 47.6, 12)
+
+p1.andar()
+p1.comer()
+p1.dormir()
