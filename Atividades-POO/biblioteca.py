6cacd37889d974a37d24c2bb3afdd28828bb71ec
Lucas Alves <64313623+Lucas7Alves@users.noreply.github.com>
2024-10-11 20:16:59 -0300

    Desafio Anda, come e dorme

--- Atividades-POO/biblioteca.py
+++ Atividades-POO/biblioteca.py
@@ -1,19 +1,67 @@
 class Pessoa:
-    def __init__(self, nome,peso,idade,comendo=False):
+    def __init__(self, nome,peso,idade):
         self.nome = nome
         self.peso = peso
         self.idade = idade
+        self.stsComer = False
+        self.stsAndar = False
+        self.stsDormir = False
 
     def andar(self):
-        print(f"{self.nome} está andando!")
+        if self.stsAndar == True:
+            print(f"{self.nome} já esta andando, não pode andar novamente")
+        elif self.stsComer == True:
+            print(f"{self.nome} não pode andar pois está comendo")
+        elif self.stsDormir == True:
+            print(f"{self.nome} não pode andar pois está dormindo")
+        else:
+            self.stsAndar = True        
+            print(f"{self.nome} está andando")
+            
 
     def comer(self):
-        if
-        self.comer = True
-        print(f"{self.nome} está comendo!")
+        if self.stsComer == True:
+            print(f"{self.nome} já esta comendo, não pode comer novamente")
+        elif self.stsAndar == True:
+            print(f"{self.nome} não pode comer pois está andando")
+        elif self.stsDormir == True:
+            print(f"{self.nome} não pode comer pois está dormindo")
+        else:
+            self.stsComer = True        
+            print(f"{self.nome} está comendo")
+            
+            
 
     def dormir(self):
-        print(f"{self.nome} está dormindo!")
+        if self.stsDormir == True:
+            print(f"{self.nome} já esta dormindo, não pode dormir novamente")
+        elif self.stsAndar == True:
+            print(f"{self.nome} não pode dormir pois está andando")
+        elif self.stsComer == True:
+            print(f"{self.nome} não pode dormir pois está comendo")
+        else:
+            self.stsDormir = True        
+            print(f"{self.nome} está dormindo")    
 
-    def pararDormir(self):
-        print(f"")
+
+
+    def pararAndar(self):
+        if self.stsAndar == True:
+            self.stsAndar = False
+            print(f"{self.nome} parou de andar")
+        else:
+            print(f"{self.nome} não está andando")
+    
+    def pararComer(self):
+        if self.stsComer == True:
+            self.stsComer = False
+            print(f"{self.nome} parou de comer")
+        else:
+            print(f"{self.nome} não está comendo")
+
+    def pararDormindo(self):
+        if self.stsDormir == True:
+            self.stsDormir = False
+            print(f"{self.nome} parou de dormir")
+        else:
+            print(f"{self.nome} não está dormir")
--- Atividades-POO/main.py
+++ Atividades-POO/main.py
@@ -1,7 +1,11 @@
 from biblioteca import *
 
-p1 = Pessoa("Lucas", 47.6, 12)
+p1 = Pessoa("Pimpão", 47.6, 12)
 
 p1.andar()
 p1.comer()
+p1.pararAndar()
+p1.comer()
+p1.dormir()
+p1.pararComer()
 p1.dormir()
